69550ae3259f490f1e86077bb13c95936971f925
Vladislav Yakovlev <red.scorpix@gmail.com>
2013-11-07 20:32:52 +0400

    Refactoring. Module Deps Writer added.

--- closurebuilder.js
+++ closurebuilder.js
@@ -1,11 +1,13 @@
 var builder = require('./lib/builder');
 var depsWriter = require('./lib/depswriter');
 var moduleBuilder = require('./lib/modulebuilder');
+var moduleDepsWriter = require('./lib/moduledepswriter');
 var requireChecker = require('./lib/requirechecker');
 
 module.exports = {
   builder: builder,
   depsWriter: depsWriter,
   moduleBuilder: moduleBuilder,
+  moduleDepsWriter: moduleDepsWriter,
   requireChecker: requireChecker
 };
